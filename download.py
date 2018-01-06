8522bc77e57ada26ba7a5fe466ff9f5edd7ff37c
wenth009 <wenth009@umn.edu>
2018-01-06 15:41:26 -0600

    Tracks nextlinks, but doesn't write properly
    Completed XPath expressions, but inserts \n between all p tags in the body.

--- download.py
+++ download.py
@@ -4,15 +4,18 @@ import sys
 
 #URL of first page is first system argument, if present 
 url=''
+output='error.html'
 try:
 	url=sys.argv[1]
+	output=sys.argv[2]
 except:
-	url='No'
+	url=''
+	print("Invalid paramaters")
 
 #Hardcoded Test Values
 titlePath='//h1[@class="entry-title"]'
 bodyPath='//div[@class="entry-content"]'
-nextPath='//div[@class="entry-content"]//a'
+nextPath='//div[@class="entry-content"]//p//a[contains(@title, "Next") or contains(text(), "Next")]'
 XPath=True
 
 #main download mechanism
@@ -25,8 +28,7 @@ while url:
 	#Fix malformed urls
 	if('http' not in url):
 		url=''
-
-	print(url)
+		print('http error')
 
 	#Get page content
 	doc=requests.get(url)
@@ -39,40 +41,47 @@ while url:
 
 		#Pull convert to tree
 		tree = html.fromstring(doc.content)
+		
 		#Get title and body, choosing XPath or CSSSelector
 		#For most items, the first item in each list is the correct one. Adjust list indecies accordingly
 		if XPath:
 			title=tree.xpath(titlePath)[0]
 			body = tree.xpath(bodyPath)[0]
+			nextLink = tree.xpath(nextPath)
 		else:
-			title=tree.cssselect('h1.entry-title')[0]
-			body = tree.cssselect('div.entry-content')[0]
+			title=tree.cssselect(titlePath)[0]
+			body = tree.cssselect(bodyPath)[0]
+			nextLink = tree.cssselect(nextPath)
 		
-		print(type(title))
 		print(title.text_content())
+
+		#Get the Next URL, as cleaning can remove the links
+		if(len(nextLink)!=0):
+			url=nextLink[0].get('href')
+		else:
+			url=''
+			print('next error')
 
 		#Clean up body
 		body.remove(body.xpath('.//div')[0])	#Removes last div tag in body, usually contains sharing links
 		
 		links=(body.xpath('.//p[.//a]'))			#Gets all links in paragraph tags
 		#Removes all paragraph tags that contain said links.
-		for a in links:
-			#print(html.tostring(a))
+		for p in links:
+			#print(html.tostring(p))
 			#print()
-			body.remove(a)
+			body.remove(p)
 
 		#Print main text of body
 		#print(html.tostring(body))
 
 		#Populate TOC and Body
-		toc+="<a href=\"#chap"+str(index)+'\"'+str(html.tostring(title))+'</a><br>'
-		bookBody+="h1 id=\"chap"+str(index)+'"'+str(html.tostring(title))+'</h1>'
-		bookBody+=str(html.tostring(body))
+		toc+='<a href="#chap'+str(index)+'">'+title.text_content()+'</a><br>'
+		bookBody+='<h1 id="chap'+str(index)+'">'+title.text_content()+'</h1>'
+		#bookBody+=str(html.tostring(body))
+		bookBody+=str(b''.join((map(html.tostring, body.iterchildren()))))
 
-		#Get URL of next chapter
-		for p in links:
-			a=p.xpath('.//a[contains(@title, "Next")]')
-			print(a[0].get('href'))
-		url2=tree.xpath(nextPath)
-		print(url2)
-		url=''
+		index+=1
+
+with open(output,'w') as f:
+	f.write(toc+'\n'+bookBody)
